57a1ada8afcffb42a8c33c1c4a82798cd05d1ec2
Shubham Rajput <rajput.shubh1993@gmail.com>
2022-10-12 16:56:26 +0530

    Added basic validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 
+  validates :name, presence: true
+  validates :age, numericality: true
+
   enum status: { active: 0, archived: 1 }
 
   scope :status_filter, ->(status_key) { where(status: User.statuses[status_key]) }
