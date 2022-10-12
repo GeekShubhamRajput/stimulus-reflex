f64a9fcd34d69bcebe4d40fbc73e9236ffe339d3
Shubham Rajput <rajput.shubh1993@gmail.com>
2022-10-12 17:03:09 +0530

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+usernames = %w{Shubham Vijay Ravi Chintu}
+usernames.each do |name|
+  User.create(name: name, age: 20, status: User.statuses.keys.sample)
+end
+puts "Users created successfully"
